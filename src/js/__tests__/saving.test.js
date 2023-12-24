c6902e586a2e0175714bd7f6bc23ba3cf9fcb615
LeaveThePast <Ramil.Budagli@yandex.ru>
2023-12-24 14:23:51 +0400

    Initial commit

---
+++ src/js/__tests__/saving.test.js
@@ -0,0 +1,17 @@
+import { GameSaving, GameSavingLoader } from "../saving";
+
+describe('GameSavingLoader', () => {
+  it('should load game saving data', async () => {
+    const saving = await GameSavingLoader.load();
+    expect(saving).toEqual({
+      id: 9,
+      created: 1546300800,
+      userInfo: {
+        id: 1,
+        name: 'Hitman',
+        level: 10,
+        points: 2000,
+      },
+    });
+  });
+});
